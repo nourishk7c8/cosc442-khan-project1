2446cce57112bbb5055250d7c807833e7de99f46
nourishk7c8 <nkhan2@students.towson.edu>
2016-02-20 20:37:39 -0500

    Project 1 - Task 14 Javadoc generated for Monopoly

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/iOwnable.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/iOwnable.java
@@ -1,9 +1,23 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface iOwnable.
+ */
 public interface iOwnable {
 
+	/**
+	 * Gets the owner.
+	 *
+	 * @return the owner
+	 */
 	Player getOwner();
 
+	/**
+	 * Sets the owner.
+	 *
+	 * @param owner the new owner
+	 */
 	void setOwner(Player owner);
 
 }
